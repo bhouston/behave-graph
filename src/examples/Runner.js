2d3c6f9c3c17b1df1d3044135b33a55926fea50b
Ben Houston <bhouston@threekit.com>
2022-08-02 16:24:41 -0400

    add more exports.  fix filename error.

--- src/examples/Runner.js
+++ src/examples/Runner.js
@@ -1,11 +1,12 @@
-import * as fs from "fs/promises";
+import * as fs from 'fs/promises';
+
 import {
   Debug,
   GraphEvaluator,
   loadGraph,
-  registerGenericNodes,
   NodeRegistry,
-} from "../../dist/index.js";
+  registerGenericNodes,
+} from '../../dist/index.js';
 
 async function main() {
   const nodeRegistry = new NodeRegistry();
@@ -13,21 +14,21 @@ async function main() {
 
   const graphJsonPath = process.argv[2];
   if (graphJsonPath === undefined) {
-    throw new Error("no path specified");
+    throw new Error('no path specified');
   }
 
   Debug.log(`reading behavior graph: ${graphJsonPath}`);
-  const textFile = await fs.readFile(graphJsonPath, { encoding: "utf-8" });
+  const textFile = await fs.readFile(graphJsonPath, { encoding: 'utf-8' });
   const graph = loadGraph(JSON.parse(textFile), nodeRegistry);
   graph.name = graphJsonPath;
 
-  Debug.log("creating behavior graph");
+  Debug.log('creating behavior graph');
   const graphEvaluator = new GraphEvaluator(graph);
 
-  Debug.log("triggering start event");
-  graphEvaluator.triggerEvents("event/start");
+  Debug.log('triggering start event');
+  graphEvaluator.triggerEvents('event/start');
 
-  Debug.log("executing all (async)");
+  Debug.log('executing all (async)');
   await graphEvaluator.executeAllAsync();
 }
 
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,23 @@
-export { default as Debug } from "./Debug";
+export { default as Debug } from './Debug';
 
-export { default as GraphEvaluator } from "./Graphs/GraphEvaluator";
-export { default as loadGraph } from "./Graphs/loadGraph";
+// main data model
+export { default as Graph } from './Graphs/Graph';
+export { default as Node } from './Nodes/Node';
+export { NodeFactory } from './Nodes/NodeFactory';
+export { default as NodeSocketRef } from './Nodes/NodeSocketRef';
+export { default as Socket } from './Sockets/Socket';
+export { SocketValueType } from './Sockets/SocketValueType';
+export { SocketFactory } from './Sockets/SocketFactory';
+export { default as FlowSocket } from './Sockets/Typed/FlowSocket';
+export { default as BooleanSocket } from './Sockets/Typed/BooleanSocket';
+export { default as NumberSocket } from './Sockets/Typed/NumberSocket';
+export { default as StringSocket } from './Sockets/Typed/StringSocket';
 
-export { default as NodeRegistry } from "./Nodes/NodeRegistry";
-export { default as registerGenericNodes } from "./Nodes/Generic/GenericNodes";
+// loading & execution
+export { default as GraphEvaluator } from './Graphs/GraphEvaluator';
+export { default as NodeEvalContext } from './Nodes/NodeEvalContext';
+export { default as loadGraph } from './Graphs/loadGraph';
+
+// node registry
+export { default as NodeRegistry } from './Nodes/NodeRegistry';
+export { default as registerGenericNodes } from './Nodes/Generic/GenericNodes';

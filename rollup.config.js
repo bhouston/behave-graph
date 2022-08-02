bd1c299464e3674c47c193372d3c5f10d66832f3
Stuart Lee <stuart.lee@fruugo.com>
2022-08-02 13:14:58 +0100

    add rollup config to bundle into single esm module

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import del from "rollup-plugin-delete";
+
+import pkg from "./package.json";
+
+export default [
+  {
+    input: "src/lib/index.ts",
+    output: [
+      {
+        file: pkg.module,
+        format: "esm",
+      },
+    ],
+    plugins: [commonjs(), typescript(), del({ targets: "dist/*" })],
+  },
+  {
+    input: "dist/lib/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [
+      dts(),
+      del({ targets: ["dist/lib", "dist/examples"], hook: "buildEnd" }),
+    ],
+  },
+];
---
+++ src/lib/index.ts
@@ -0,0 +1,5 @@
+export { default as GraphEvaluator } from "./Graphs/GraphEvaluator";
+export { default as loadGraph } from "./Graphs/loadGraph";
+
+export { default as NodeRegistry } from "./Nodes/NodeRegistry";
+export { default as registerGenericNodes } from "./Nodes/Generic/GenericNodes";

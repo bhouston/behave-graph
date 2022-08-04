d287e0ba81edebbb26fe1c3e306e7672b4b9bcf4
Ben Houston <bhouston@threekit.com>
2022-08-03 23:25:20 -0400

    add ultra simple web example.

---
+++ src/examples/commandline/index.ts
@@ -0,0 +1,37 @@
+import * as fs from 'fs/promises';
+
+import {
+  Debug,
+  GraphEvaluator,
+  GraphTypeRegistry,
+  readGraphFromJSON,
+  registerGenericNodes,
+} from '../../../dist/lib/index';
+
+async function main() {
+  const registry = new GraphTypeRegistry();
+  registerGenericNodes(registry);
+
+  const graphJsonPath = process.argv[2];
+  if (graphJsonPath === undefined) {
+    throw new Error('no path specified');
+  }
+
+  Debug.log(`reading behavior graph: ${graphJsonPath}`);
+  const textFile = await fs.readFile(graphJsonPath, { encoding: 'utf-8' });
+  const graph = readGraphFromJSON(JSON.parse(textFile), registry);
+  graph.name = graphJsonPath;
+
+  // await fs.writeFile('./examples/test.json', JSON.stringify(writeGraphToJSON(graph), null, ' '), { encoding: 'utf-8' });
+
+  Debug.log('creating behavior graph');
+  const graphEvaluator = new GraphEvaluator(graph);
+
+  Debug.log('triggering start event');
+  graphEvaluator.triggerEvents('event/start');
+
+  Debug.log('executing all (async)');
+  await graphEvaluator.executeAllAsync();
+}
+
+main();
--- src/examples/simpleweb/index.ts
+++ src/examples/simpleweb/index.ts
@@ -6,20 +6,22 @@ import {
   GraphTypeRegistry,
   readGraphFromJSON,
   registerGenericNodes,
-} from '../../dist/lib/index';
+} from '../../../dist/lib/index';
 
 async function main() {
   const registry = new GraphTypeRegistry();
   registerGenericNodes(registry);
 
-  const graphJsonPath = process.argv[2];
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const exampleName = urlSearchParams.get("exampleName");
+  const graphJsonPath = `/examples/basics/${exampleName}.json`;
   if (graphJsonPath === undefined) {
     throw new Error('no path specified');
   }
 
   Debug.log(`reading behavior graph: ${graphJsonPath}`);
-  const textFile = await fs.readFile(graphJsonPath, { encoding: 'utf-8' });
-  const graph = readGraphFromJSON(JSON.parse(textFile), registry);
+  const json = await (await fetch(graphJsonPath)).json();
+  const graph = readGraphFromJSON(json, registry);
   graph.name = graphJsonPath;
 
   // await fs.writeFile('./examples/test.json', JSON.stringify(writeGraphToJSON(graph), null, ' '), { encoding: 'utf-8' });

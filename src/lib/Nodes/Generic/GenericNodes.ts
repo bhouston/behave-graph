20b94d9d8ac4abe8c2e7b8b442b6610ee0f7083d
Ben Houston <bhouston@threekit.com>
2022-08-03 20:56:25 -0400

    fix spelling mistake: Uniary -> Unary

--- src/lib/Nodes/Generic/GenericNodes.ts
+++ src/lib/Nodes/Generic/GenericNodes.ts
@@ -11,7 +11,7 @@ import ForLoop from './Flow/ForLoop';
 import Sequence from './Flow/Sequence';
 import BinaryOp from './Logic/BinaryOp';
 import NullaryOp from './Logic/NullaryOp';
-import UniaryOp from './Logic/UniaryOp';
+import UnaryOp from './Logic/UnaryOp';
 import StateExists from './State/StateExists';
 import StateGet from './State/StateGet';
 import StateSet from './State/StateSet';
@@ -40,13 +40,13 @@ export default function registerGenericNodes(registry: GraphTypeRegistry) {
 
   // logic: constants
 
-  registry.registerNodeType('logic/booleanConstant', () => new UniaryOp<boolean, boolean>('logic/booleanConstant', (a) => (a)));
-  registry.registerNodeType('logic/numberConstant', () => new UniaryOp<number, number>('logic/numberConstant', (a) => (a)));
-  registry.registerNodeType('logic/stringConstant', () => new UniaryOp<string, string>('logic/stringConstant', (a) => (a)));
+  registry.registerNodeType('logic/booleanConstant', () => new UnaryOp<boolean, boolean>('logic/booleanConstant', (a) => (a)));
+  registry.registerNodeType('logic/numberConstant', () => new UnaryOp<number, number>('logic/numberConstant', (a) => (a)));
+  registry.registerNodeType('logic/stringConstant', () => new UnaryOp<string, string>('logic/stringConstant', (a) => (a)));
 
   // logic: boolean logic
 
-  registry.registerNodeType('logic/booleanNot', () => new UniaryOp<boolean, boolean>('logic/booleanNot', (a) => (!a)));
+  registry.registerNodeType('logic/booleanNot', () => new UnaryOp<boolean, boolean>('logic/booleanNot', (a) => (!a)));
   registry.registerNodeType('logic/booleanOr', () => new BinaryOp<boolean, boolean>('logic/booleanOr', (a, b) => (a || b)));
   registry.registerNodeType('logic/booleanAnd', () => new BinaryOp<boolean, boolean>('logic/booleanAnd', (a, b) => (a && b)));
 
@@ -57,37 +57,37 @@ export default function registerGenericNodes(registry: GraphTypeRegistry) {
   registry.registerNodeType('logic/numberMultiply', () => new BinaryOp<number, number>('logic/numberMultiply', (a, b) => (a * b)));
   registry.registerNodeType('logic/numberDivide', () => new BinaryOp<number, number>('logic/numberDivide', (a, b) => (a / b)));
   registry.registerNodeType('logic/numberPow', () => new BinaryOp<number, number>('logic/numberPow', (a, b) => (a ** b)));
-  registry.registerNodeType('logic/numberNegate', () => new UniaryOp<number, number>('logic/numberNegate', (a) => (-a)));
-  registry.registerNodeType('logic/numberSqrt', () => new UniaryOp<number, number>('logic/numberSqrt', (a) => (Math.sqrt(a))));
+  registry.registerNodeType('logic/numberNegate', () => new UnaryOp<number, number>('logic/numberNegate', (a) => (-a)));
+  registry.registerNodeType('logic/numberSqrt', () => new UnaryOp<number, number>('logic/numberSqrt', (a) => (Math.sqrt(a))));
 
   // logic: exponential
 
   registry.registerNodeType('logic/numberE', () => new NullaryOp<number>('logic/numberSign', () => (Math.E)));
-  registry.registerNodeType('logic/numberExp', () => new UniaryOp<number, number>('logic/numberExp', (a) => (Math.exp(a))));
-  registry.registerNodeType('logic/numberLog', () => new UniaryOp<number, number>('logic/numberLog', (a) => (Math.log(a))));
-  registry.registerNodeType('logic/numberLog2', () => new UniaryOp<number, number>('logic/numberLog2', (a) => (Math.log2(a))));
-  registry.registerNodeType('logic/numberLog10', () => new UniaryOp<number, number>('logic/numberLog10', (a) => (Math.log10(a))));
+  registry.registerNodeType('logic/numberExp', () => new UnaryOp<number, number>('logic/numberExp', (a) => (Math.exp(a))));
+  registry.registerNodeType('logic/numberLog', () => new UnaryOp<number, number>('logic/numberLog', (a) => (Math.log(a))));
+  registry.registerNodeType('logic/numberLog2', () => new UnaryOp<number, number>('logic/numberLog2', (a) => (Math.log2(a))));
+  registry.registerNodeType('logic/numberLog10', () => new UnaryOp<number, number>('logic/numberLog10', (a) => (Math.log10(a))));
 
   // logic: trigonometry
 
   registry.registerNodeType('logic/numberPi', () => new NullaryOp<number>('logic/numberNegate', () => (Math.PI)));
-  registry.registerNodeType('logic/numberSin', () => new UniaryOp<number, number>('logic/numberSin', (a) => (Math.sin(a))));
-  registry.registerNodeType('logic/numberAsin', () => new UniaryOp<number, number>('logic/numberSin', (a) => (Math.asin(a))));
-  registry.registerNodeType('logic/numberCos', () => new UniaryOp<number, number>('logic/numberCos', (a) => (Math.cos(a))));
-  registry.registerNodeType('logic/numberAcos', () => new UniaryOp<number, number>('logic/numberCos', (a) => (Math.acos(a))));
-  registry.registerNodeType('logic/numberTan', () => new UniaryOp<number, number>('logic/numberTan', (a) => (Math.tan(a))));
-  registry.registerNodeType('logic/numberAtan', () => new UniaryOp<number, number>('logic/numberTan', (a) => (Math.atan(a))));
+  registry.registerNodeType('logic/numberSin', () => new UnaryOp<number, number>('logic/numberSin', (a) => (Math.sin(a))));
+  registry.registerNodeType('logic/numberAsin', () => new UnaryOp<number, number>('logic/numberSin', (a) => (Math.asin(a))));
+  registry.registerNodeType('logic/numberCos', () => new UnaryOp<number, number>('logic/numberCos', (a) => (Math.cos(a))));
+  registry.registerNodeType('logic/numberAcos', () => new UnaryOp<number, number>('logic/numberCos', (a) => (Math.acos(a))));
+  registry.registerNodeType('logic/numberTan', () => new UnaryOp<number, number>('logic/numberTan', (a) => (Math.tan(a))));
+  registry.registerNodeType('logic/numberAtan', () => new UnaryOp<number, number>('logic/numberTan', (a) => (Math.atan(a))));
 
   // logic: ranges
 
   registry.registerNodeType('logic/numberMin', () => new BinaryOp<number, number>('logic/numberMin', (a, b) => (Math.min(a, b))));
   registry.registerNodeType('logic/numberMax', () => new BinaryOp<number, number>('logic/numberMax', (a, b) => (Math.max(a, b))));
-  registry.registerNodeType('logic/numberSign', () => new UniaryOp<number, number>('logic/numberSign', (a) => (Math.sign(a))));
-  registry.registerNodeType('logic/numberAbs', () => new UniaryOp<number, number>('logic/numberAbs', (a) => (Math.abs(a))));
-  registry.registerNodeType('logic/numberFloor', () => new UniaryOp<number, number>('logic/numberFloor', (a) => (Math.floor(a))));
-  registry.registerNodeType('logic/numberCeil', () => new UniaryOp<number, number>('logic/numberCeil', (a) => (Math.ceil(a))));
-  registry.registerNodeType('logic/numberRound', () => new UniaryOp<number, number>('logic/numberCeil', (a) => (Math.round(a))));
-  registry.registerNodeType('logic/numberTrunc', () => new UniaryOp<number, number>('logic/numberTrunc', (a) => (Math.trunc(a))));
+  registry.registerNodeType('logic/numberSign', () => new UnaryOp<number, number>('logic/numberSign', (a) => (Math.sign(a))));
+  registry.registerNodeType('logic/numberAbs', () => new UnaryOp<number, number>('logic/numberAbs', (a) => (Math.abs(a))));
+  registry.registerNodeType('logic/numberFloor', () => new UnaryOp<number, number>('logic/numberFloor', (a) => (Math.floor(a))));
+  registry.registerNodeType('logic/numberCeil', () => new UnaryOp<number, number>('logic/numberCeil', (a) => (Math.ceil(a))));
+  registry.registerNodeType('logic/numberRound', () => new UnaryOp<number, number>('logic/numberCeil', (a) => (Math.round(a))));
+  registry.registerNodeType('logic/numberTrunc', () => new UnaryOp<number, number>('logic/numberTrunc', (a) => (Math.trunc(a))));
 
   // logic: sampling
 
@@ -100,15 +100,15 @@ export default function registerGenericNodes(registry: GraphTypeRegistry) {
   registry.registerNodeType('logic/numberGreaterThaOrEqual', () => new BinaryOp<number, boolean>('logic/numberGreaterThaOrEqual', (a, b) => (a >= b)));
   registry.registerNodeType('logic/numberLessThan', () => new BinaryOp<number, boolean>('logic/numberLessThan', (a, b) => (a < b)));
   registry.registerNodeType('logic/numberLessThaOrEqual', () => new BinaryOp<number, boolean>('logic/numberLessThaOrEqual', (a, b) => (a <= b)));
-  registry.registerNodeType('logic/numberIsNan', () => new UniaryOp<number, boolean>('logic/numberIsNan', (a) => (Number.isNaN(a))));
-  registry.registerNodeType('logic/numberIsInf', () => new UniaryOp<number, boolean>('logic/numberIsInf', (a) => ((!Number.isFinite(a)) && !Number.isNaN(a))));
+  registry.registerNodeType('logic/numberIsNan', () => new UnaryOp<number, boolean>('logic/numberIsNan', (a) => (Number.isNaN(a))));
+  registry.registerNodeType('logic/numberIsInf', () => new UnaryOp<number, boolean>('logic/numberIsInf', (a) => ((!Number.isFinite(a)) && !Number.isNaN(a))));
 
   // logic: string utilities
 
   registry.registerNodeType('logic/stringConcat', () => new BinaryOp<string, string>('logic/stringConcat', (a, b) => (a.concat(b))));
   registry.registerNodeType('logic/stringIncludes', () => new BinaryOp<string, boolean>('logic/stringIncludes', (a, b) => (a.includes(b))));
-  registry.registerNodeType('logic/numberToString', () => new UniaryOp<number, string>('logic/numberToString', (a) => (a.toString())));
-  registry.registerNodeType('logic/stringLength', () => new UniaryOp<string, number>('logic/stringLength', (a) => (a.length)));
+  registry.registerNodeType('logic/numberToString', () => new UnaryOp<number, string>('logic/numberToString', (a) => (a.toString())));
+  registry.registerNodeType('logic/stringLength', () => new UnaryOp<string, number>('logic/stringLength', (a) => (a.length)));
 
   // state
 
--- src/lib/Nodes/Generic/Logic/UnaryOp.ts
+++ src/lib/Nodes/Generic/Logic/UnaryOp.ts
@@ -2,7 +2,7 @@ import NumberSocket from '../../../Sockets/Typed/NumberSocket';
 import Node from '../../Node';
 import NodeEvalContext from '../../NodeEvalContext';
 
-export default class UniaryOp<Input, Output> extends Node {
+export default class UnaryOp<Input, Output> extends Node {
   constructor(nodeName: string, public uniaryEvalFunc: (a: Input) => Output) {
     super(
       nodeName,

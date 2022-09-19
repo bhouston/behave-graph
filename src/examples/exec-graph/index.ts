64dec28e4397ad926b32e0fcf8374bc90afce023
Ben Houston <bhouston@threekit.com>
2022-09-19 14:52:24 -0400

    disable execution logging by default

--- src/examples/exec-graph/index.ts
+++ src/examples/exec-graph/index.ts
@@ -1,7 +1,7 @@
 import { promises as fs } from 'fs';
 
 import {
-  Debug, GraphEvaluator, GraphRegistry, Node, NodeEvaluationType, readGraphFromJSON, registerGenericNodes,
+  Debug, GraphEvaluator, GraphRegistry, readGraphFromJSON, registerGenericNodes,
   validateDirectedAcyclicGraph, validateGraphRegistry, validateLinks,
 } from '../../../dist/lib/index';
 
@@ -42,6 +42,7 @@ async function main() {
   Debug.logVerbose('creating behavior graph');
   const graphEvaluator = new GraphEvaluator(graph);
 
+  /*
   graphEvaluator.evaluationListeners.push((node: Node, nodeEvaluationType: NodeEvaluationType, async: boolean) => {
     if (nodeEvaluationType === NodeEvaluationType.None) {
       console.log(`Node ${node.typeName} ${node.id} completed evaluation.`);
@@ -49,6 +50,7 @@ async function main() {
       console.log(`Node ${node.typeName} ${node.id} started evaluation, mode: ${NodeEvaluationType[nodeEvaluationType]}, async: ${async}.`);
     }
   });
+  */
 
   Debug.logVerbose('triggering start event');
   graphEvaluator.triggerEvents('event/start');

7289860a22ebae0aa9daacf31ffa7ec1d2aa0bd3
Ben Houston <bhouston@threekit.com>
2022-09-23 19:12:33 -0400

    improving binaryop and adding TernaryOp.

--- src/lib/Nodes/Generic/Logic/TernaryOp.ts
+++ src/lib/Nodes/Generic/Logic/TernaryOp.ts
@@ -2,13 +2,14 @@ import createTypedSocket from '../../../Sockets/Typed/TypedSocketFactory';
 import Node from '../../Node';
 import NodeEvalContext from '../../NodeEvalContext';
 
-export default class BinaryOp<Input1, Input2, Output> extends Node {
+export default class Ternary<Input1, Input2, Input3, Output> extends Node {
   constructor(
     nodeName: string,
     input1ValueType: string,
     input2ValueType: string,
+    input3ValueType: string,
     outputValueType: string,
-    public binaryEvalFunc: (a: Input1, b: Input2) => Output,
+    public binaryEvalFunc: (a: Input1, b: Input2, c: Input3) => Output,
   ) {
     super(
       'Logic',
@@ -16,12 +17,13 @@ export default class BinaryOp<Input1, Input2, Output> extends Node {
       [
         createTypedSocket(input1ValueType, 'a'),
         createTypedSocket(input2ValueType, 'b'),
+        createTypedSocket(input3ValueType, 'c'),
       ],
       [
         createTypedSocket(outputValueType, 'result'),
       ],
       (context: NodeEvalContext) => {
-        context.writeOutput('result', this.binaryEvalFunc(context.readInput('a'), context.readInput('b')));
+        context.writeOutput('result', this.binaryEvalFunc(context.readInput('a'), context.readInput('b'), context.readInput('c')));
       },
     );
   }

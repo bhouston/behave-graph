b34b42ac7a0609fbed0bdfd7e438033cafdc26aa
Ben Houston <bhouston@threekit.com>
2022-10-01 13:00:07 -0400

    unify jest.config.js for threeify and behave-graph

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverageFrom: [
+    '/src/**/*.{js,jsx,ts}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+  ],
+  rootDir: './src'
 };

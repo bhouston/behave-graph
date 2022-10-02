37ac22bcffcf69415d75089ad68a15507ffba59c
Ben Houston <neuralsoft@gmail.com>
2022-10-01 23:28:58 -0400

    rename eslint, format json files.

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,6 @@
 export default {
+  name: "behave-graph",
   preset: 'ts-jest',
   testEnvironment: 'node',
-  collectCoverageFrom: [
-    '/src/**/*.{js,jsx,ts}',
-    '!**/node_modules/**',
-    '!**/vendor/**',
-  ],
   rootDir: './src'
 };

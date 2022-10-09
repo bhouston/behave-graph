8b84bd1a8713e04d8845532f0b014218683b8a9d
Ben Houston <neuralsoft@gmail.com>
2022-10-08 22:03:02 -0400

    serialized bigint as a number if it is within the integer range.

--- src/lib/Values/ValueTypeRegistry.ts
+++ src/lib/Values/ValueTypeRegistry.ts
@@ -36,7 +36,10 @@ export class ValueTypeRegistry {
         'integer',
         () => 0n,
         (value: string | number): bigint => BigInt(value),
-        (value: bigint) => value.toString() // prefer string to ensure full range is covered
+        (value: bigint) =>
+          Number.MIN_SAFE_INTEGER <= value && value <= Number.MAX_SAFE_INTEGER
+            ? Number(value)
+            : value.toString() // prefer string to ensure full range is covered
       )
     );
     this.register(

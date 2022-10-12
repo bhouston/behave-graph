a6234aecce94102f1b26265afbb9e6552c5a5b46
Ben Houston <bhouston@threekit.com>
2022-10-12 18:31:45 -0400

    expose mix.  split out scene value registrations.  remove generic from set/get scene property.

---
+++ src/lib/Profiles/Scene/registerEulerValue.ts
@@ -0,0 +1,106 @@
+import { In1Out1FuncNode } from '../../Nodes/Templates/In1Out1FuncNode.js';
+import { In2Out1FuncNode } from '../../Nodes/Templates/In2Out1FuncNode.js';
+import { In3Out1FuncNode } from '../../Nodes/Templates/In3Out1FuncNode.js';
+import { Registry } from '../../Registry.js';
+import { ValueType } from '../../Values/ValueType.js';
+import { VecCreate } from './Logic/VecCreate.js';
+import { VecElements } from './Logic/VecElements.js';
+import {
+  Vec3,
+  vec3Add,
+  vec3FromArray,
+  Vec3JSON,
+  vec3Mix,
+  vec3Parse,
+  vec3ToArray,
+  vec3ToString
+} from './Values/Vec3.js';
+import { eulerToQuat, Vec4 } from './Values/Vec4.js';
+
+export function registerEulerValue(registry: Registry) {
+  const { nodes, values } = registry;
+
+  values.register(
+    new ValueType(
+      'euler',
+      () => new Vec3(),
+      (value: string | Vec3JSON) =>
+        typeof value === 'string'
+          ? vec3Parse(value)
+          : new Vec3(value.x, value.y, value.z),
+      (value) => ({ x: value.x, y: value.y, z: value.z } as Vec3JSON)
+    )
+  );
+
+  // logic: euler
+
+  nodes.register(
+    'logic/create/euler',
+    () =>
+      new VecCreate<Vec3>(
+        'logic/create/euler',
+        'euler',
+        ['x', 'y', 'z'],
+        (elements: number[]) => vec3FromArray(elements)
+      )
+  );
+  nodes.register(
+    'logic/euler',
+    () =>
+      new In1Out1FuncNode<Vec3, Vec3>('logic/euler', 'euler', 'euler', (a) => a)
+  );
+  nodes.register(
+    'logic/elements/euler',
+    () =>
+      new VecElements<Vec3>(
+        'logic/elements/euler',
+        'euler',
+        ['x', 'y', 'z'],
+        vec3ToArray
+      )
+  );
+
+  nodes.register(
+    'logic/add/euler',
+    () =>
+      new In2Out1FuncNode<Vec3, Vec3, Vec3>(
+        'logic/add/euler',
+        'euler',
+        'euler',
+        'euler',
+        (a, b) => vec3Add(a, b)
+      )
+  );
+  nodes.register(
+    'logic/mix/euler',
+    () =>
+      new In3Out1FuncNode<Vec3, Vec3, number, Vec3>(
+        'logic/mix/euler',
+        'euler',
+        'euler',
+        'float',
+        'euler',
+        (a, b, c) => vec3Mix(a, b, c)
+      )
+  );
+  nodes.register(
+    'logic/toString/euler',
+    () =>
+      new In1Out1FuncNode<Vec3, string>(
+        'logic/toString/euler',
+        'euler',
+        'string',
+        (a) => vec3ToString(a)
+      )
+  );
+  nodes.register(
+    'logic/toQuat/euler',
+    () =>
+      new In1Out1FuncNode<Vec3, Vec4>(
+        'logic/toQuat/euler',
+        'euler',
+        'quat',
+        (a) => eulerToQuat(a)
+      )
+  );
+}

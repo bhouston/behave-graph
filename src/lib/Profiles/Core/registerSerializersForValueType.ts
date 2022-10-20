3cee139cb4ea49f0874c089adbff0b6fb91c741f
Ben Houston <neuralsoft@gmail.com>
2022-10-19 21:50:35 -0400

    more bug fixing.

--- src/lib/Profiles/Core/registerSerializersForValueType.ts
+++ src/lib/Profiles/Core/registerSerializersForValueType.ts
@@ -1,3 +1,4 @@
+import { NodeDescription } from '../../Nodes/NodeDescription.js';
 import { In1Out1FuncNode } from '../../Nodes/Templates/In1Out1FuncNode.js';
 import { Registry } from '../../Registry.js';
 import { toCamelCase } from '../../toCamelCase.js';
@@ -6,26 +7,33 @@ export function registerSerializersForValueType(
   registry: Registry,
   valueTypeName: string
 ) {
+  const camelCaseValueTypeName = toCamelCase(valueTypeName);
   registry.nodes.register(
-    `logic/to${toCamelCase(valueTypeName)}/string`,
-    (graph, nodeType) =>
-      new In1Out1FuncNode<string, any>(
-        graph,
-        nodeType,
-        'string',
-        valueTypeName,
-        (a: string) => registry.values.get(valueTypeName).deserialize(a)
-      )
-  );
-  registry.nodes.register(
-    `logic/toString/${valueTypeName}`,
-    (graph, nodeType) =>
-      new In1Out1FuncNode<any, string>(
-        graph,
-        nodeType,
-        valueTypeName,
-        'string',
-        (a: any) => registry.values.get(valueTypeName).serialize(a)
-      )
+    new NodeDescription(
+      `math/to${camelCaseValueTypeName}/string`,
+      'Logic',
+      `To ${camelCaseValueTypeName}`,
+      (graph, nodeType) =>
+        new In1Out1FuncNode<string, any>(
+          graph,
+          nodeType,
+          'string',
+          valueTypeName,
+          (a: string) => registry.values.get(valueTypeName).deserialize(a)
+        )
+    ),
+    new NodeDescription(
+      `math/toString/${valueTypeName}`,
+      'Logic',
+      'To String',
+      (graph, nodeType) =>
+        new In1Out1FuncNode<any, string>(
+          graph,
+          nodeType,
+          valueTypeName,
+          'string',
+          (a: any) => registry.values.get(valueTypeName).serialize(a)
+        )
+    )
   );
 }

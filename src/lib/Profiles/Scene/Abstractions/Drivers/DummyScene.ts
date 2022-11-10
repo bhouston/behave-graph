fdbed23c76c698bf6152f05f9622b56f17576f57
Ben Houston <bhouston@threekit.com>
2022-11-10 13:54:16 -0500

    make DummyScene independent of registry

--- src/lib/Profiles/Scene/Abstractions/Drivers/DummyScene.ts
+++ src/lib/Profiles/Scene/Abstractions/Drivers/DummyScene.ts
@@ -1,14 +1,38 @@
-import { EventEmitter } from '../../lib/Events/EventEmitter.js';
-import { IScene } from '../../lib/Profiles/Scene/Abstractions/IScene.js';
-import { Registry } from '../../lib/Registry.js';
+import { EventEmitter } from '../../../../Events/EventEmitter.js';
+import { ValueTypeRegistry } from '../../../../Values/ValueTypeRegistry.js';
+import { BooleanValue } from '../../../Core/Values/BooleanValue.js';
+import { FloatValue } from '../../../Core/Values/FloatValue.js';
+import { IntegerValue } from '../../../Core/Values/IntegerValue.js';
+import { StringValue } from '../../../Core/Values/StringValue.js';
+import { ColorValue } from '../../Values/ColorValue.js';
+import { EulerValue } from '../../Values/EulerValue.js';
+import { QuatValue } from '../../Values/QuatValue.js';
+import { Vec2Value } from '../../Values/Vec2Value.js';
+import { Vec3Value } from '../../Values/Vec3Value.js';
+import { Vec4Value } from '../../Values/Vec4Value.js';
+import { IScene } from '../IScene.js';
 
 export class DummyScene implements IScene {
   public onSceneChanged = new EventEmitter<void>();
+  private valueRegistry = new ValueTypeRegistry();
 
-  constructor(public registry: Registry) {}
+  constructor() {
+    const values = this.valueRegistry;
+    // pull in value type nodes
+    values.register(BooleanValue);
+    values.register(StringValue);
+    values.register(IntegerValue);
+    values.register(FloatValue);
+    values.register(Vec2Value);
+    values.register(Vec3Value);
+    values.register(Vec4Value);
+    values.register(ColorValue);
+    values.register(EulerValue);
+    values.register(QuatValue);
+  }
 
   getProperty(jsonPath: string, valueTypeName: string): any {
-    return this.registry.values.get(valueTypeName).creator();
+    return this.valueRegistry.get(valueTypeName).creator();
   }
   setProperty(): void {
     this.onSceneChanged.emit();

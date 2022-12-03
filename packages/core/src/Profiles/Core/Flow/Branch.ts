f75fdc441e7ceffefde807b0636561a9f40b9b0f
Ben Houston <neuralsoft@gmail.com>
2022-12-03 18:07:09 -0500

    enable help descriptions to nodes.

--- packages/core/src/Profiles/Core/Flow/Branch.ts
+++ packages/core/src/Profiles/Core/Flow/Branch.ts
@@ -1,16 +1,21 @@
 import { Fiber } from '../../../Execution/Fiber';
 import { Graph } from '../../../Graphs/Graph';
 import { FlowNode } from '../../../Nodes/FlowNode';
-import { NodeDescription } from '../../../Nodes/Registry/NodeDescription';
+import {
+  NodeDescription,
+  NodeDescription2
+} from '../../../Nodes/Registry/NodeDescription';
 import { Socket } from '../../../Sockets/Socket';
 
 export class Branch extends FlowNode {
-  public static Description = new NodeDescription(
-    'flow/branch',
-    'Flow',
-    'Branch',
-    (description, graph) => new Branch(description, graph)
-  );
+  public static Description = new NodeDescription2({
+    typeName: 'flow/branch',
+    category: 'Flow',
+    label: 'Branch',
+    factory: (description, graph) => new Branch(description, graph),
+    helpDescription:
+      "Checks the value of the 'condition' input and if true, executes the 'true' branch, otherwise it executes the 'false' branch."
+  });
 
   constructor(description: NodeDescription, graph: Graph) {
     super(

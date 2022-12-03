459714a0b58bd10faf2dd14b9ff395fd93db3389
Ben Houston <neuralsoft@gmail.com>
2022-12-03 18:07:16 -0500

    remove use of .at

--- packages/core/src/toCamelCase.ts
+++ packages/core/src/toCamelCase.ts
@@ -1,6 +1,6 @@
 export function toCamelCase(text: string): string {
   if (text.length > 0) {
-    return text.at(0)?.toLocaleUpperCase() + text.slice(1);
+    return text.slice(0, 1).toLocaleUpperCase() + text.slice(1);
   }
   return text;
 }

febd75a18e3a1bc863db41fef5c7d10bf8ab9bec
Ben Houston <neuralsoft@gmail.com>
2022-12-16 21:32:20 -0500

    adopt arrays in BooleanNodes.

--- packages/core/src/Profiles/Core/Values/BooleanNodes.ts
+++ packages/core/src/Profiles/Core/Values/BooleanNodes.ts
@@ -11,47 +11,47 @@ export const Constant = new FunctionNodeDesc({
 export const And = new FunctionNodeDesc({
   name: 'math/and/boolean',
   label: '∧',
-  in: { a: 'boolean', b: 'boolean' },
-  out: { result: 'boolean' },
+  in: ['boolean', 'boolean'],
+  out: 'boolean',
   exec: (a: boolean, b: boolean) => a && b
 });
 
 export const Or = new FunctionNodeDesc({
   name: 'math/or/boolean',
   label: '∨',
-  in: { a: 'boolean', b: 'boolean' },
-  out: { result: 'boolean' },
+  in: ['boolean', 'boolean'],
+  out: 'boolean',
   exec: (a: boolean, b: boolean) => a || b
 });
 
 export const Not = new FunctionNodeDesc({
   name: 'math/negate/boolean',
   label: '¬',
-  in: { a: 'boolean' },
-  out: { result: 'boolean' },
+  in: ['boolean'],
+  out: 'boolean',
   exec: (a: boolean) => !a
 });
 
 export const ToFloat = new FunctionNodeDesc({
   name: 'math/toFloat/boolean',
   label: 'To Float',
-  in: { a: 'boolean' },
-  out: { result: 'float' },
+  in: ['boolean'],
+  out: 'float',
   exec: (a: boolean) => (a ? 1 : 0)
 });
 
 export const Equal = new FunctionNodeDesc({
   name: 'math/equal/boolean',
   label: '=',
-  in: { a: 'boolean', b: 'boolean' },
-  out: { result: 'boolean' },
+  in: ['boolean', 'boolean'],
+  out: 'boolean',
   exec: (a: boolean, b: boolean) => a === b
 });
 
 export const toInteger = new FunctionNodeDesc({
   name: 'math/toInteger/boolean',
   label: 'To Integer',
-  in: { a: 'boolean' },
-  out: { result: 'integer' },
+  in: ['boolean'],
+  out: 'integer',
   exec: (a: boolean) => (a ? 1n : 0n)
 });

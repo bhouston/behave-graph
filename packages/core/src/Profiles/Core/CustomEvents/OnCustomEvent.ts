2047dfb5be85628efe4861fca23cd215cc595bad
Dan Oved <stangogh@gmail.com>
2022-12-26 18:48:41 -0800

    migrated onCustomEvent

--- packages/core/src/Profiles/Core/CustomEvents/OnCustomEvent.ts
+++ packages/core/src/Profiles/Core/CustomEvents/OnCustomEvent.ts
@@ -1,111 +1,78 @@
-import { NodeConfiguration } from 'packages/core/src/Nodes/Node';
-
 import { Assert } from '../../../Diagnostics/Assert';
 import { CustomEvent } from '../../../Events/CustomEvent';
-import { Engine } from '../../../Execution/Engine';
-import { Graph } from '../../../Graphs/Graph';
-import { EventNode2 } from '../../../Nodes/EventNode';
 import {
-  NodeDescription,
-  NodeDescription2
-} from '../../../Nodes/Registry/NodeDescription';
-import { Socket } from '../../../Sockets/Socket';
+  makeEventNodeDefinition,
+  NodeCategory,
+  SocketsList
+} from '../../../Nodes/NodeDefinition';
 
-// export const OnCustomEvent = makeEventNodeDefinition({
-//   name: 'customEvent/onTriggered',
-//   category: NodeCategory.Event,
-//   description: 'On Triggered',
-//   configuration: {
-//     customEventId: {
-//       valueType: 'number'
-//     }
-//   },
-//   out: (configuration, graph: { customEvents }) => {
-//     const customEvent =
-//       customEvents[configuration.customEventId] ||
-//       new CustomEvent('-1', 'undefined');
-//     return {
-//       sockets: {
-//         flow: 'flow',
+type State = {
+  onCustomEvent: (params: Record<string, any>) => void;
+  customEvent: CustomEvent;
+};
 
-//       },
-//       keys: ['flow']
-//     };
-//   }
-// });
+const makeInitialState = (): State | undefined => undefined;
 
-export class OnCustomEventOric extends EventNode2 {
-  public static Description = new NodeDescription2({
-    typeName: 'customEvent/onTriggered',
-    category: 'Event',
-    label: 'On Triggered',
-    configuration: {
-      customEventId: {
-        valueType: 'number'
-      }
-    },
-    factory: (description, graph, configuration) =>
-      new OnCustomEventOric(description, graph, configuration)
-  });
-
-  private readonly customEvent: CustomEvent;
-
-  constructor(
-    description: NodeDescription,
-    graph: Graph,
-    configuration: NodeConfiguration
-  ) {
+export const OnCustomEvent = makeEventNodeDefinition({
+  typeName: 'customEvent/onTriggered',
+  category: NodeCategory.Event,
+  label: 'On Triggered',
+  configuration: {
+    customEventId: {
+      valueType: 'number'
+    }
+  },
+  in: {},
+  out: (configuration, graph) => {
+    const customEvents = graph.customEvents;
     const customEvent =
-      graph.customEvents[configuration.customEventId] ||
+      customEvents[configuration.customEventId] ||
       new CustomEvent('-1', 'undefined');
-    super({
-      description,
-      graph,
-      outputs: [
-        new Socket('flow', 'flow'),
-        ...customEvent.parameters.map(
-          (parameter) =>
-            new Socket(
-              parameter.valueTypeName,
-              parameter.name,
-              parameter.value,
-              parameter.label
-            )
-        )
-      ],
-      configuration
-    });
-    this.customEvent = customEvent;
-  }
-  private onCustomEvent:
-    | ((parameters: { [parameter: string]: any }) => void)
-    | undefined = undefined;
 
-  init(engine: Engine) {
-    Assert.mustBeTrue(this.onCustomEvent === undefined);
+    const result: SocketsList = [
+      {
+        key: 'flow',
+        valueType: 'flow'
+      },
+      ...customEvent.parameters.map((parameter) => ({
+        key: parameter.name,
+        valueType: parameter.valueTypeName,
+        label: parameter.label
+      }))
+    ];
 
-    this.onCustomEvent = (parameters) => {
-      this.customEvent.parameters.forEach((parameterSocket) => {
+    return result;
+  },
+  initialState: makeInitialState(),
+  init: ({ write, commit, state, configuration, graph: { customEvents } }) => {
+    Assert.mustBeTrue(state === undefined);
+
+    const customEvent = customEvents[configuration.customEventId];
+
+    const onCustomEvent = (parameters: Record<string, any>) => {
+      customEvent.parameters.forEach((parameterSocket) => {
         if (!(parameterSocket.name in parameters)) {
           throw new Error(
             `parameters of custom event do not align with parameters of custom event node, missing ${parameterSocket.name}`
           );
         }
-        this.writeOutput(
-          parameterSocket.name,
-          parameters[parameterSocket.name]
-        );
+        write(parameterSocket.name, parameters[parameterSocket.name]);
       });
-      engine.commitToNewFiber(this, 'flow');
+      commit('flow');
     };
-    this.customEvent.eventEmitter.addListener(this.onCustomEvent);
-  }
+    customEvent.eventEmitter.addListener(onCustomEvent);
 
-  dispose(engine: Engine) {
-    Assert.mustBeTrue(this.onCustomEvent !== undefined);
+    // return updated state, so that we can later in dispose detach the listener from the event
+    return {
+      customEvent,
+      onCustomEvent
+    };
+  },
+  dispose: ({ state }) => {
+    Assert.mustBeTrue(state !== undefined);
 
-    if (this.onCustomEvent !== undefined) {
-      this.customEvent.eventEmitter.removeListener(this.onCustomEvent);
+    if (typeof state !== 'undefined') {
+      state.customEvent.eventEmitter.removeListener(state.onCustomEvent);
     }
   }
-}
+});

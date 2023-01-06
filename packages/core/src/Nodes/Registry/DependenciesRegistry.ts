60344c29663dfb9e7e5315126f7f224010600758
Ben Houston <neuralsoft@gmail.com>
2023-01-06 10:15:25 -0500

    validate dependencies are not registered twice.

--- packages/core/src/Nodes/Registry/DependenciesRegistry.ts
+++ packages/core/src/Nodes/Registry/DependenciesRegistry.ts
@@ -2,12 +2,15 @@ export class DependenciesRegistry {
   private readonly registryKeyToDependency: { [key: string]: any } = {};
 
   register(key: string, dependency: any) {
+    if (key in this.registryKeyToDependency) {
+      throw new Error(`already registered dependency with name '${key}`);
+    }
     this.registryKeyToDependency[key] = dependency;
   }
 
   get<T>(key: string): T {
     if (!(key in this.registryKeyToDependency)) {
-      throw new Error(`can not find depenency with name '${key}`);
+      throw new Error(`can not find dependency with name '${key}`);
     }
     return this.registryKeyToDependency[key];
   }
